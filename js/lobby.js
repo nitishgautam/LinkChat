59260018572a774b24d65f7ae9b49acb7b215ac5
Nitish <nitishgautam78@gmail.com>
2023-02-14 20:31:54 +0530

    Add files via upload

---
+++ js/lobby.js
@@ -0,0 +1,21 @@
+let form = document.getElementById('lobby__form');
+
+// Setting Name /Change the storage from session to localstorage later
+let displayName = sessionStorage.getItem('display_name');
+if(displayName){
+    form.name.value = displayName;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    sessionStorage.setItem('display_name', e.target.name.value);
+
+    let inviteCode = e.target.room.value;
+    if(!inviteCode){
+        inviteCode = String(Math.floor(Math.random() * 10000))
+    }
+
+    window.location = `index.html?room=${inviteCode}`;
+});
+
